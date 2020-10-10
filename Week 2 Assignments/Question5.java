50b4cf2a0a1e1dee580d00a7837abe9ef2df63fc
Infinity <45239108+Akhila-Krishna@users.noreply.github.com>
2020-10-11 01:24:07 +0530

    Create Question5.java

---
+++ Week 2 Assignments/Question5.java
@@ -0,0 +1,12 @@
+// Complete the code segment to debug / complete the program which is intended to print 'NPTEL JAVA'.
+
+public class Question215{ 
+    public static void main(String[] args) { 
+        //Declare variable with name 'nptel', 'space' and 'java' and proper datatype.
+        //Initialize the variables with proper input
+        String nptel = "NPTEL";
+        String space = " ";
+        String java = "JAVA";
+        System.out.print(nptel+space+java);
+   }
+}
