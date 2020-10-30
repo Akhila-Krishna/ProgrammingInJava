9b1fcc208d368a06b0486f931da395a4ec71a7fe
Infinity <45239108+Akhila-Krishna@users.noreply.github.com>
2020-10-30 21:10:39 +0530

    Update Question1.java

--- Week 8 Assignments/Question1.java
+++ Week 8 Assignments/Question1.java
@@ -6,15 +6,15 @@ public class Pattern1 {
         Scanner inr = new Scanner(System.in);
 		int n = inr.nextInt();
         
-      	int k = 0,sum=0;
-        for(int i = 1; i <= n; ++i, k = 0) {
-            for(int space = 1; space <= n - i; ++space) {
+      	int k = 0, sum=0;
+        for(int i = 1; i <= n; i++, k=0) {
+            for(int space = 1; space <= n - i; space++) {
                 System.out.print("  ");
             }
             while(k != 2 * i - 1) {
                 System.out.print("* ");
-                sum=sum+1;
-                ++k;
+                sum = sum+1;
+                k++;
             }
             System.out.println();
         }
