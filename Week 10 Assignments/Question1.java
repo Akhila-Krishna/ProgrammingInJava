0867ee26ce4573f08a015008f520d67c3f62db98
Infinity <45239108+Akhila-Krishna@users.noreply.github.com>
2020-11-16 13:31:47 +0530

    Create Question1.java

---
+++ Week 10 Assignments/Question1.java
@@ -0,0 +1,22 @@
+// The following code needs some package to work properly. 
+// Write appropriate code to import the required package(s) in order to make the program compile and execute successfully.
+
+import java.sql.*;
+import java.lang.*;
+
+public class Question101 {
+    public static void main(String args[]) {
+        try {
+              Connection conn = null;
+              Statement stmt = null;
+              String DB_URL = "jdbc:sqlite:/tempfs/db";
+              System.setProperty("org.sqlite.tmpdir", "/tempfs");
+              
+              conn = DriverManager.getConnection(DB_URL);
+              System.out.print(conn.isValid(1));
+              conn.close();
+
+        }
+       catch(Exception e){ System.out.println(e);}  
+    }
+}
